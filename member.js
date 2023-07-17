eb1fd49158a8fb0dc5be69697e3838437c00e3d5
gkumik <gkumik@gmail.com>
2023-07-17 21:16:12 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembers() {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/components/members/skills-members.html',
+    controller: 'SkillsMembersController',
+    controllerAs: 'skillsMembers'
+  };
+}
